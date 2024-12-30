3a6a4f9ff9c71b12fac356967c62a83fe454279b
mhlee <lee.munhaeng@gmail.com>
2024-12-30 13:58:49 +0900

    exception message 필드 추가

--- src/main/java/me/mhlee/mentoring/mentoring/common/exceptions/WarningException.java
+++ src/main/java/me/mhlee/mentoring/mentoring/common/exceptions/WarningException.java
@@ -1,4 +1,12 @@
 package me.mhlee.mentoring.mentoring.common.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
 public class WarningException extends RuntimeException {
+    private String message;
 }
