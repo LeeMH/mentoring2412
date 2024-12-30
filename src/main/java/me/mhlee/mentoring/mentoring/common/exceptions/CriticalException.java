b4c2391f6db03d3ec63b0db37060a026dc20b2bf
mhlee <lee.munhaeng@gmail.com>
2024-12-30 10:33:56 +0900

    exception 3종 추가

---
+++ src/main/java/me/mhlee/mentoring/mentoring/common/exceptions/CriticalException.java
@@ -0,0 +1,4 @@
+package me.mhlee.mentoring.mentoring.common.exceptions;
+
+public class CriticalException extends RuntimeException {
+}
---
+++ src/main/java/me/mhlee/mentoring/mentoring/common/exceptions/WarningException.java
@@ -0,0 +1,4 @@
+package me.mhlee.mentoring.mentoring.common.exceptions;
+
+public class WarningException extends RuntimeException {
+}
