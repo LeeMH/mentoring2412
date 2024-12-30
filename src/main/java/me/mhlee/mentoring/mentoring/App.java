cdff51e86e183b681f5baa860f101838277dcded
mhlee <lee.munhaeng@gmail.com>
2024-12-30 09:08:24 +0900

    메인파일명 변경

--- src/main/java/me/mhlee/mentoring/mentoring/App.java
+++ src/main/java/me/mhlee/mentoring/mentoring/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MentoringApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MentoringApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
